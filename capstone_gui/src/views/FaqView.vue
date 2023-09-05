8a14b13909fb46382bf3725bb4ca2a21c9a01226
Aphelele2 <aphelelejoyi@gmail.com>
2023-09-05 16:21:01 +0200

    FAQs

--- capstone_gui/src/views/FaqView.vue
+++ capstone_gui/src/views/FaqView.vue
@@ -1,15 +1,76 @@
 <template>
-    <div>
-        <h1>FAQs</h1>
+    <div class="faqs-container">
+      <header class="faqs-header">
+        <h1 class="faqs-title">Frequently Asked Questions</h1>
+      </header>
+  
+      <section class="faq-item">
+        <h2 class="faq-question">Q1: What is The Knowledge Hub?</h2>
+        <p class="faq-answer">
+          The Knowledge Hub is an innovative online library system tailored for township schools. It's designed to better manage everything in school libraries and provide easy access to educational resources.
+        </p>
+      </section>
+  
+      <section class="faq-item">
+        <h2 class="faq-question">Q2: Why was The Knowledge Hub created?</h2>
+        <p class="faq-answer">
+          The Knowledge Hub was created to address the challenges faced by students in township schools, where libraries were often disorganized. The goal is to make library resources easily accessible and improve the overall educational experience.
+        </p>
+      </section>
+  
+      <section class="faq-item">
+        <h2 class="faq-question">Q3: What are the key features of The Knowledge Hub?</h2>
+        <p class="faq-answer">
+          The key features include an effortless book search, an easy checkout process, real-time book availability checks, personalization options, and a comprehensive catalog of books covering various subjects and interests.
+        </p>
+      </section>
+  
+      <section class="faq-item">
+        <h2 class="faq-question">Q4: How can I join The Knowledge Hub?</h2>
+        <p class="faq-answer">
+          To join The Knowledge Hub and benefit from its resources, simply sign up on our website or contact your school's library administrator for more information.
+        </p>
+      </section>
     </div>
-</template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <script>
+  export default {
+    
+  }
+  </script>
+  
+  <style scoped>
+  .faqs-container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  
+  .faqs-header {
+    background-color: #007BFF;
+    color: #FFF;
+    padding: 20px;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  
+  .faqs-title {
+    font-size: 36px;
+  }
+  
+  .faq-item {
+    margin-bottom: 40px;
+  }
+  
+  .faq-question {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+  
+  .faq-answer {
+    font-size: 18px;
+    line-height: 1.5;
+  }
+  </style>
+  
