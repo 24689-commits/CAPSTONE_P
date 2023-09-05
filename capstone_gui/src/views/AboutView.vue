0adcaf6af33549b20c861faec663043cc96d5d25
Aphelele2 <aphelelejoyi@gmail.com>
2023-09-05 16:09:17 +0200

    about

--- capstone_gui/src/views/AboutView.vue
+++ capstone_gui/src/views/AboutView.vue
@@ -1,5 +1,90 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <header>
+      <h1>The Knowledge Hub</h1>
+      <p>An Online Library System for Township Schools</p>
+    </header>
+
+    <section class="why-section">
+      <h2>Why Choose The Knowledge Hub?</h2>
+      <p>
+        Welcome to The Knowledge Hub, where knowledge meets innovation, and where we are committed to transforming the way libraries operate in township schools.
+      </p>
+    </section>
+
+    <section class="vision-section">
+      <h2>Our Vision</h2>
+      <p>
+        At The Knowledge Hub, we believe that access to information and education should be effortless and empowering. We envision a world where every student, regardless of their background, can tap into the wealth of knowledge available in their school libraries, fostering a lifelong love for learning.
+      </p>
+    </section>
+
+    <section class="story-section">
+      <h2>Our Story</h2>
+      <p>
+        The Knowledge Hub was born out of a deep desire to address the challenges faced by students in township schools. Our founder, who experienced the frustration of disorganized libraries during their high school years, decided to take action. He recognized the need for a modern, user-friendly library system that could make students' lives easier and ensure they have access to the resources they need to thrive academically.
+      </p>
+    </section>
+
+    <section class="mission-section">
+      <h2>Our Mission</h2>
+      <p>
+        Our mission is simple yet profound: to revolutionize the library experience for township schools. We aim to provide a dynamic, web-based library system that streamlines book management, promotes literacy, and empowers both students and educators.
+      </p>
+    </section>
+
+    <section class="join-section">
+      <h2>Join Us in the Knowledge Revolution</h2>
+      <p>
+        We invite you to embark on this journey with us as we bring The Knowledge Hub Library System to life. Together, we can empower the next generation with the tools they need to succeed in their educational pursuits. Join us in shaping a brighter future through knowledge, accessibility, and innovation.
+      </p>
+    </section>
   </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+header {
+  background-color: #007BFF;
+  color: #FFF;
+  padding: 20px;
+}
+
+header h1 {
+  justify-content: center;
+  font-size: 36px;
+}
+
+header p {
+  font-size: 18px;
+}
+
+section {
+  margin: 40px 0;
+}
+
+h2 {
+  font-size: 28px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  font-size: 18px;
+  margin: 10px 0;
+}
+</style>
